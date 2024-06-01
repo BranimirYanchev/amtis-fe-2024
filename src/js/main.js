dccd3063e46504ff3a0366f6bac0ee46d935ba8a
BranimirYanchev <branimiryanchev@gmail.com>
2024-06-01 10:26:54 +0300

    changes v1

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-// Продължи с твоя vanilla JavaScript код тук.
+let xhttp = new XMLHttpRequest();
+
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       alert(http.responceText);
+    }
+};
+
+xhttp.open("GET", "../db/file.json", true);
+xhttp.send();
